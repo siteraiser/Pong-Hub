411750168f5f2b94581d8768d99ad79122d3aff4
Carl Turechek <carl@siteraiser.com>
2024-04-26 15:10:37 -0400

    Update order.php

--- controllers/order.php
+++ controllers/order.php
@@ -3,7 +3,7 @@ class order extends requestHandler{
 
 	public function index(){
 		$ia_id = $this->url_segments[1];
-		$data['title']="Order Product";
+	
 		$data['meta']="
 		<style>
 		html,body{pdding:0;margin:0;border:0;}
@@ -41,13 +41,90 @@ class order extends requestHandler{
 		#address input,#uuid{margin:5px;}
 		#copy_section{padding:5px;}
 		#senddata{padding:5px;}
+		
+    #physical_goods_shipping {
+        border: solid 2px black;
+        padding: 4px;
+    }
+
+
+   #submission_type_selection > * {
+    vertical-align:middle;
+   }
+/* The switch - the box around the slider */
+.switch {
+  position: relative;
+  display: inline-block;
+  width: 60px;
+  height: 34px;
+}
+
+/* Hide default HTML checkbox */
+.switch input {
+  opacity: 0;
+  width: 0;
+  height: 0;
+}
+
+/* The slider */
+.slider {
+  position: absolute;
+  cursor: pointer;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background-color: #42d12f;
+  -webkit-transition: .4s;
+  transition: .4s;
+}
+
+.slider:before {
+  position: absolute;
+  content: \"\";
+  height: 26px;
+  width: 26px;
+  left: 4px;
+  bottom: 4px;
+  background-color: #fff;
+  -webkit-transition: .4s;
+  transition: .4s;
+}
+
+input:checked + .slider {
+  background-color: #2196F3;
+}
+
+input:focus + .slider {
+  box-shadow: 0 0 1px #2196F3;
+}
+
+input:checked + .slider:before {
+  -webkit-transform: translateX(26px);
+  -ms-transform: translateX(26px);
+  transform: translateX(26px);
+}
+
+/* Rounded sliders */
+.slider.round {
+  border-radius: 34px;
+}
+
+.slider.round:before {
+  border-radius: 50%;
+}
+
+
+
 		</style>";
 		$data['description']="";
 		$data['keywords']="";		
 
 		$this->loadModel('productModel');	
 	    $product_results = $this->productModel->getFullProduct($ia_id);	
-	
+		
+		$data['title']="Order Product: ".htmlspecialchars($product_results['label']);
+		
 		$data['iaddress']=$product_results;
 		$data['iaddresses']=$this->productModel->getIntegratedAddressesByPid($product_results['user'],$product_results['product_id']);
 	/*	echo'<pre>';
